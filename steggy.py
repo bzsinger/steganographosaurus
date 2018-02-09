5f7dd32d8818e860d0ea73ce05b4d497213a81a9
Akash Kwatra <akashkw@gmail.com>
2018-02-08 21:44:05 -0600

    Try to create key image

--- steggy.py
+++ steggy.py
@@ -1,8 +1,15 @@
 import os, sys
+import random
 from PIL import Image
 
 stegoboi = Image.open("./stegosaurus.png")
+width, height = stegoboi.size
+#random.randint(0,1)
+key = [[(255,255,255) for x in range(width)] for y in range(height)]
 
+keypic = Image.new('RGB', (width, height), "red")
+keypic.putdata(key)
+keypic.save("key.bmp")
 
 if (__name__ == "__main__"):
-    print(stegoboi.size)
+    print(key)
