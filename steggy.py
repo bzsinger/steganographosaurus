bc27f59e1f38941485f0ba655f3f6645684b5ba7
Drothknar <sueanne@cs.utexas.edu>
2018-02-11 13:33:35 -0600

    wrote frombits for decoding

--- steggy.py
+++ steggy.py
@@ -13,14 +13,14 @@ keypic.save("key.bmp")
 
 msg = "Hello World!"
 def tobits(msg):
-    arr = bytearray(msg, 'ascii')
-    results = []
-    for i in arr:
-        results.append(bin(i)[2:])
-    return results
+    return [bin(i)[2:] for i in bytearray(msg, 'ascii')]
+
+#Need to cite 
+def frombits(bin_list):
+    return ''.join([chr(int(x, 2)) for x in bin_list])
 
 
 if (__name__ == "__main__"):
-
     print tobits(msg)
-    #print(key)
+    result = tobits(msg)
+    print frombits(result)  
