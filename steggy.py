4ac18fbf618bcd2a7bfaec20d26b7b5bfd73dc22
Drothknar <sueanne@cs.utexas.edu>
2018-02-11 17:38:47 -0600

    Cleaned up code, added comments

--- steggy.py
+++ steggy.py
@@ -19,24 +19,33 @@ def tobits(msg):
 #Need to cite 
 def frombits(bin_list):
     return ''.join([chr(int(x, 2)) for x in bin_list])
-"""
-pseudocode:
-    For every (x, y) flattened pixel, we check if keypic is true
-    If so, the last two bits of every color of (x, y) will be modified
 
-    getpixel(x, y)
-    putpixel(x, y)
-"""
-def write_stego(bin_list):
+def write_stego(key, bin_list):
     bit_place = 0
+    #Go through each pixel, and if the pixel is off in the key, place a bit
+    #in the last part of the pixel
     for x in range(height):
         for y in range(width): 
-            if key[y + x*width] == 1 and bit_place < len(bin_list):            
+            if key[y + x*width] == 0 and bit_place < len(bin_list):            
                 pixelO = stego2boi.getpixel((x,y))
                 pixelO = tuple([((i >> 1) << 1) + int(bin_list[bit_place]) for i in pixelO])
                 bit_place +=1
                 stego2boi.putpixel((x,y), pixelO) 
     stego2boi.save("stego2.bmp")
+
+def decode_stego(key, filename):
+    bit_place = 0;
+    bin_list = [];
+    numOne = key.count(1);
+    stegoPic = Image.open(filename)
+    for x in range(height):
+        for y in range(width): 
+            #put this in a function to break out of it
+            if key[y + x*width] == 0 and len(bin_list) < numOne:            
+                pixelO = stego2boi.getpixel((x,y))
+                bin_list.append((pixelO[0] %2 != 0))
+                bit_place +=1
+    return frombits(bin_list) 
 
 if (__name__ == "__main__"):
     print tobits(msg)
