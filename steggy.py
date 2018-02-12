615f9253c34230d753d08a69fc2e4912375899ec
Akash Kwatra <akashkw@gmail.com>
2018-02-11 23:54:32 -0600

    Completed CLI

--- steggy.py
+++ steggy.py
@@ -7,13 +7,17 @@ from keyio import read_key, generate_key
 
 
 if(__name__ == "__main__"):
-    print("\nWelcome to Jurassic Park!!\n")
-    print("\nToday we will visit the stegonographasurus :D\n")
-    print("\nThen we will visit our security team, Dennis Nedry, to talk about how to keep the park running smoothly ;) \n")
-    time.sleep(1000)
-    print("\nSo here is the steganographastorus! Legend has it, he will keep secrets for you :O\n")
+    time.sleep(1)
+    print("\nWelcome to Jurassic Park!!")
+    time.sleep(2)
+    print("\nToday we will visit the stegonographasurus :D")
+    time.sleep(2)
+    print("\nThen we will visit our security team, Dennis Nedry, to talk about how to keep the park running smoothly ;)")
+    time.sleep(3)
+    print("\n\nSo here is the steganographastorus! Legend has it, he will keep secrets for you :O")
+    time.sleep(2)
     print("\nWould you like to tell him something? Or is is there something he has for you?\n")
-
+    time.sleep(2)
 
     while (True):
         operation = input("*Type encrypt or decrypt pls me too thanks* :: ").upper()
@@ -28,18 +32,18 @@ if(__name__ == "__main__"):
     while (True):
 
         if operation == 'E':
-            key_name = read_key(input("\nWhat would you like to name the key file :: "))
-            output_name = read_key(input("\nWhat would you like to name the encrypted image :: "))
-            image_file = read_key(input("\nWhat image file would you like to encrypt? :: "))
-            message = read_key(input("\nWhat is your message? :: "))
+            key_name = input("\nWhat would you like to name the key file :: ")
+            output_name = input("\nWhat would you like to name the encrypted image :: ")
+            image_file = input("\nWhat image file would you like to encrypt? :: ")
+            message = input("\nWhat is your message? :: ")
             print("\n\nGenerating Random Key...\n")
-            time.sleep(1000)
+            time.sleep(1)
             generate_key(image_file, message, key_name)
             print("\nKey saved as {}\n".format(key_name))
             key = read_key(key_name)
-            time.sleep(500)
+            time.sleep(2)
             print("\nEncrypting image...\n")
-            time.sleep(500)
+            time.sleep(2)
             write_stego(key, message, image_file, output_name)
             print("\nEncrypted image saved as {}\n".format(output_name))
             break
@@ -47,10 +51,14 @@ if(__name__ == "__main__"):
         else:
             key = read_key(input("\nPlease enter key file name :: "))
             image_name = input("\nPlease enter encrypted image name :: ")
+            time.sleep(2)
+            print("\nDecrypting {}\n".format(image_name))
+            time.sleep(2)
             print("\n\nYour message is :: {}".format(decode_stego(key, image_name)))
             break
 
         print("\nI'm sorry what was that??\n")
-    
+    time.sleep(2)
     print("\nI hope you had a wonderful time visiting the steganographotsowetwnwknlenwwrjoos!!!\n")
+    time.sleep(2)
     print("\nGoodbye!\n")
